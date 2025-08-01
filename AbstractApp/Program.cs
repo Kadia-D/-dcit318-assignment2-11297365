de9900fc46e818f9678b17f0259bb3300ce7b6ae
Kadia-D <kddellor@gmail.com>
2025-08-01 17:51:05 +0000

    Abstract app

---
+++ AbstractApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AbstractApp
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(4);
+            Shape rectangle = new Rectangle(9, 5);
+
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
+}
